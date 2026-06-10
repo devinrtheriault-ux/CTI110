6082f73e95198363ac7dd8962aa54a5f1e04e0bd
devinrtheriault-ux <devin.r.theriault@gmail.com>
2026-06-10 13:07:58 -0400

    Add files via upload

---
+++ P1LAB1_TheriaultDevin.py
@@ -0,0 +1,8 @@
+# Devin Theriault
+# 6/10/2026
+# P1LAB1
+# CTI110/Python Program/input-output
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Hello, " + first_name + " " + last_name + "! Welcome to CTI-110")
